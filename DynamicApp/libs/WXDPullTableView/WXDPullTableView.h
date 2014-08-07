bd384b106ad4c9ff452e852d3b17137bf1e2038d
kuolei <leikuo@yahoo.com>
2014-08-07 18:55:42 +0800

    增加PullTableView
    增加PullTableView，但功能还未编写，请期待。

---
+++ DynamicApp/libs/WXDPullTableView/WXDPullTableView.h
@@ -0,0 +1,13 @@
+//
+//  WXDPullTableView.h
+//  Protoshop
+//
+//  Created by kuolei on 8/7/14.
+//  Copyright (c) 2014 kuolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXDPullTableView : UITableView
+
+@end
