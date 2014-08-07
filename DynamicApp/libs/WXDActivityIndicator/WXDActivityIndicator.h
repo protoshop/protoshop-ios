d0629ddba75e65469e5f253dca11951b10e76aad
kuolei <leikuo@yahoo.com>
2014-08-07 18:54:46 +0800

    添加自定义indicatorView
    添加自定义indicatorView

---
+++ DynamicApp/libs/WXDActivityIndicator/WXDActivityIndicator.h
@@ -0,0 +1,18 @@
+//
+//  WXDActivityIndicator.h
+//  Protoshop
+//
+//  Created by kuolei on 8/7/14.
+//  Copyright (c) 2014 kuolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXDActivityIndicator : UIView
+
+-(void) clear;
+-(void) step;
+-(void) startAnimation;
+-(void) stopAnimation;
+
+@end
